bbd14fda5974b5fb23f836f42475e72841ea4919
sidey79 <7968127+sidey79@users.noreply.github.com>
2022-06-19 18:23:11 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+umask 077;
+mkdir -p /opt/template-ca/output
+mkdir -p /opt/template-ca/crl
+mkdir -p /opt/template-ca/private
+mkdir -p /opt/template-ca/newcerts
+    
 export OPENSSL_CONF=./root-ca.cnf
 
 while :; do
