9d6d39dabc2e20b66b05f77c8a2c5488350f3053
sidey79 <7968127+sidey79@users.noreply.github.com>
2022-06-19 18:32:47 +0200

    Update run.sh
    Added basic initalisation to template ca

--- run.sh
+++ run.sh
@@ -8,6 +8,15 @@ mkdir -p /opt/template-ca/newcerts
     
 export OPENSSL_CONF=/opt/rootca.cnf
 
+indexfile=/opt/template-ca/root-ca.index
+test -f $indexfile || touch $indexfile
+
+crlfile=/opt/template-ca/root-ca.crlnum
+test -f $crlfile || echo 00 > $crlfile
+
+serialfile=/opt/template-ca/root-ca.serial
+test -f $serialfile || openssl rand -hex 16 > $serialfile
+
 while :; do
     sleep 1
 done
